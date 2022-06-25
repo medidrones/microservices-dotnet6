3c67c49307814269343516b332f1aecb84738f59
Jorge Medina <medidrones@gmail.com>
2022-06-25 19:56:59 -0300

    Nivelamento em REST com ASPNet

---
+++ RestWithASPNET/RestWithASPNET/Program.cs
@@ -0,0 +1,26 @@
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
